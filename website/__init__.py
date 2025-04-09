320ba1353733264ef469e55fcd1814ad3822b33f
Andres Maciel <andres.maciel@sjsu.edu>
2025-04-09 09:17:17 -0700

    Edited Login page to match gokids theme

--- website/__init__.py
+++ website/__init__.py
@@ -26,6 +26,7 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
+    login_manager.login_message = None  # Disable "Please log in..." message
     login_manager.init_app(app)
 
     @login_manager.user_loader
